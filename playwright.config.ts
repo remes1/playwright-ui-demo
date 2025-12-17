7eccfaa3dba8bf3b17460a90fcc5534aec37ce50
stanislavlivsin <s.livshyn91@gmail.com>
2025-12-17 19:11:47 +0100

    added playwright.yaml, refactoring

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,9 +22,5 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
   ],
 });
